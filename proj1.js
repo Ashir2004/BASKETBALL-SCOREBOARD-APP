16fd4c2a864cada99be3f95c639e371c90e5174c
Ashir2004 <muhammadashir2004@gmail.com>
2024-07-11 12:57:08 +0500

    Add files via upload
    Very basic project consisting of Increment function operations. The Basketball Scoreboard App using html/CSS/JS, a solo project given by Scrimba as a course task.

---
+++ proj1.js
@@ -0,0 +1,36 @@
+
+let h = document.getElementById("home")
+h=0
+//console.log(h)
+function f1(){
+    h = h + 1
+   home.textContent = h 
+   
+}
+function f2(){
+    h = h + 2
+   home.textContent = h 
+   
+}
+function f3(){
+    h = h + 3
+   home.textContent = h
+   
+}
+let g = document.getElementById("guest")
+g=0
+function s1(){
+    g = g + 1
+   guest.textContent = g 
+   
+}
+function s2(){
+    g = g + 2
+   guest.textContent = g 
+   
+}
+function s3(){
+    g = g + 3
+   guest.textContent = g
+   
+}
